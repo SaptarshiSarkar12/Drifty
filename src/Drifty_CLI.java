057943ad186e6dcd875c0a605cffa3d7128e8560
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2022-09-21 21:23:27 +0530

    Updated
    Signed-off-by: Saptarshi Sarkar <saptarshi.programmer@gmail.com>

--- src/Drifty_CLI.java
+++ src/Drifty_CLI.java
@@ -13,7 +13,6 @@ public class Drifty_CLI {
     protected static final String COLOR_BLUE = "\u001B[34m";
     protected static final String COLOR_RESET = "\u001B[0m";
     private static final Scanner SC = new Scanner(System.in);
-    protected static final String COLOR_BLUE_BOLD = "\033[1;34m";
     protected static final String COLOR_CYAN = "\u001B[36m";
     public static void main(String[] args) {
         System.out.println(COLOR_GREEN + "====================================================================" + COLOR_RESET);
@@ -28,21 +27,19 @@ public class Drifty_CLI {
         while (true) {
             System.out.print(COLOR_BLUE + "Do you want to download the file in your default downloads folder? (Enter Y for yes and N for no) : " + COLOR_RESET);
             char default_folder = SC.nextLine().toLowerCase().charAt(0);
-            switch (default_folder) {
-                case 'y' -> {
-                    downloadsFolder = DefaultDownloadFolderLocationFinder.findPath() + System.getProperty("file.separator");
-                    if (downloadsFolder == null) {
-                        System.out.println(COLOR_RED + "Failed to retrieve default download folder!" + COLOR_RESET);
-                        enterDownloadsFolder();
-                    } else {
-                        System.out.println(COLOR_PURPLE + "Default download folder detected : " + COLOR_CYAN_BOLD + downloadsFolder + COLOR_RESET);
-                    }
+            if (default_folder == 'y') {
+                downloadsFolder = DefaultDownloadFolderLocationFinder.findPath() + System.getProperty("file.separator");
+                if (downloadsFolder == null) {
+                    System.out.println(COLOR_RED + "Failed to retrieve default download folder!" + COLOR_RESET);
+                    enterDownloadsFolder();
+                } else {
+                    System.out.println(COLOR_PURPLE + "Default download folder detected : " + COLOR_CYAN_BOLD + downloadsFolder + COLOR_RESET);
                 }
-                case 'n' -> enterDownloadsFolder();
-                default -> {
-                    System.out.println(COLOR_RED + "Invalid input!" + COLOR_RESET);
-                    continue;
-                }
+            } else if (default_folder == 'n') {
+                enterDownloadsFolder();
+            } else {
+                System.out.println(COLOR_RED + "Invalid input!" + COLOR_RESET);
+                continue;
             }
             break;
         }
