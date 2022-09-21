a34961c2ba962fe604e246fd000111582cc89bf2
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2022-09-22 00:41:56 +0530

    Updated
    Signed-off-by: Saptarshi Sarkar <saptarshi.programmer@gmail.com>

--- src/Drifty_CLI.java
+++ src/Drifty_CLI.java
@@ -4,42 +4,35 @@ import java.util.Scanner;
 
 public class Drifty_CLI {
     private static String downloadsFolder;
-    protected static final String COLOR_BRIGHT_BLUE_BOLD = "\033[1;94m";
-    protected static final String COLOR_BLUE_UNDERLINED = "\033[4;34m";
-    protected static final String COLOR_RED = "\u001B[31m";
-    protected static final String COLOR_CYAN_BOLD = "\033[1;36m";
-    protected static final String COLOR_PURPLE = "\u001B[35m";
-    protected static final String COLOR_GREEN = "\u001B[32m";
-    protected static final String COLOR_BLUE = "\u001B[34m";
-    protected static final String COLOR_RESET = "\u001B[0m";
     private static final Scanner SC = new Scanner(System.in);
-    protected static final String COLOR_CYAN = "\u001B[36m";
+    private static boolean flag = false;
     public static void main(String[] args) {
-        boolean flag = false;
-        System.out.println(COLOR_GREEN + "====================================================================" + COLOR_RESET);
-        System.out.println(COLOR_BRIGHT_BLUE_BOLD + "\t\t\tDRIFTY" + COLOR_RESET);
-        System.out.println(COLOR_GREEN + "====================================================================" + COLOR_RESET);
-
-        System.out.print(COLOR_PURPLE + "Enter the link to the file : ");
+        if (!flag) {
+            System.out.println("====================================================================");
+            System.out.println("\t\t\t   DRIFTY CLI");
+            System.out.println("====================================================================");
+        }
+        flag = true;
+        System.out.print("Enter the link to the file : ");
         String link = SC.next();
         SC.nextLine();
-        System.out.print("Enter the filename (with file extension) : " + COLOR_RESET);
+        System.out.print("Enter the filename (with file extension) : ");
         String fName = SC.nextLine();
         while (true) {
-            System.out.print(COLOR_BLUE + "Do you want to download the file in your default downloads folder? (Enter Y for yes and N for no) : " + COLOR_RESET);
+            System.out.print("Do you want to download the file in your default downloads folder? (Enter Y for yes and N for no) : ");
             char default_folder = SC.nextLine().toLowerCase().charAt(0);
             if (default_folder == 'y') {
                 downloadsFolder = DefaultDownloadFolderLocationFinder.findPath() + System.getProperty("file.separator");
                 if (downloadsFolder == null) {
-                    System.out.println(COLOR_RED + "Failed to retrieve default download folder!" + COLOR_RESET);
+                    System.out.println("Failed to retrieve default download folder!");
                     enterDownloadsFolder();
                 } else {
-                    System.out.println(COLOR_PURPLE + "Default download folder detected : " + COLOR_CYAN_BOLD + downloadsFolder + COLOR_RESET);
+                    System.out.println("Default download folder detected : " + downloadsFolder);
                 }
             } else if (default_folder == 'n') {
                 enterDownloadsFolder();
             } else {
-                System.out.println(COLOR_RED + "Invalid input!" + COLOR_RESET);
+                System.out.println("Invalid input!");
                 continue;
             }
             break;
@@ -49,7 +42,7 @@ public class Drifty_CLI {
     }
 
     private static void enterDownloadsFolder(){
-        System.out.print(COLOR_PURPLE + "Enter the directory in which you want to download the file : " + COLOR_RESET);
+        System.out.print("Enter the directory in which you want to download the file : ");
         downloadsFolder = SC.nextLine().replace('/', '\\');
         if (!(downloadsFolder.endsWith("\\"))) {
             downloadsFolder = downloadsFolder + System.getProperty("file.separator");
