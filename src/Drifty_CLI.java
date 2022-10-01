ced5f6fb7f533ace7a0844ec59bbb3ad3c1ec084
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2022-10-01 11:40:30 +0530

    Trying to fix a branch conflict

--- src/Drifty_CLI.java
+++ src/Drifty_CLI.java
@@ -1,23 +1,32 @@
 package src;
 
+import java.io.IOException;
 import java.util.Scanner;
 
+import org.apache.logging.log4j.*;
+
 public class Drifty_CLI {
+    private static Logger dLog=LogManager.getLogger(Drifty_CLI.class.getName());
     private static String downloadsFolder;
     private static final Scanner SC = new Scanner(System.in);
-    private static boolean flag = false;
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    private static boolean flag = false;
     public static void main(String[] args) {
+
+        dLog.log(Level.INFO,"Application Started");
+
         if (!flag) {
-            System.out.println("====================================================================");
+
+            System.out.println(ANSI_PURPLE+"===================================================================="+ANSI_RESET);
             System.out.println(ANSI_CYAN+"  _____   _____   _____  ______  _______ __     __"+ANSI_RESET);
             System.out.println(ANSI_CYAN+" |  __ \\ |  __ \\ |_   _||  ____||__   __|\\ \\   / /"+ANSI_RESET);
             System.out.println(ANSI_CYAN+" | |  | || |__) |  | |  | |__      | |    \\ \\_/ /"+ANSI_RESET);
             System.out.println(ANSI_CYAN+" | |  | ||  _  /   | |  |  __|     | |     \\   / "+ANSI_RESET);
             System.out.println(ANSI_CYAN+" | |__| || | \\ \\  _| |_ | |        | |      | |  "+ANSI_RESET);
-            System.out.println(ANSI_CYAN+" |_____/ |_|  \\_\\|_____||_|        |_|      |_|  "+ANSI_RESET);
-            System.out.println("====================================================================");
+            System.out.println(ANSI_CYAN+" |_____/ |_|  \\_\\|_____||_|        |_|      |_|  "+ANSI_RESET);            
+            System.out.println(ANSI_PURPLE+"===================================================================="+ANSI_RESET);
         }
         flag = true;
         System.out.print("Enter the link to the file : ");
@@ -31,15 +40,18 @@ public class Drifty_CLI {
             if (default_folder == 'y') {
                 downloadsFolder = DefaultDownloadFolderLocationFinder.findPath() + System.getProperty("file.separator");
                 if (downloadsFolder == null) {
-                    System.out.println("Failed to retrieve default download folder!");
+                    //System.out.println("Failed to retrieve default download folder!");
+                    dLog.error("Failed to retrieve default download folder!");
                     enterDownloadsFolder();
                 } else {
-                    System.out.println("Default download folder detected : " + downloadsFolder);
+                   // System.out.println("Default download folder detected : " + downloadsFolder);
+                    dLog.info("Default download folder detected : " + downloadsFolder);
                 }
             } else if (default_folder == 'n') {
                 enterDownloadsFolder();
             } else {
-                System.out.println("Invalid input!");
+                //System.out.println("Invalid input!");
+                dLog.error("Invalid input");
                 continue;
             }
             break;
