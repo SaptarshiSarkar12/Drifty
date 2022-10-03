38d668858438868ee264bd811dee8a92d18f5a99
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2022-10-04 00:00:45 +0530

    Added JavaDoc in methods and classes
    Signed-off-by: Saptarshi Sarkar <saptarshi.programmer@gmail.com>

--- src/ProgressBarThread.java
+++ src/ProgressBarThread.java
@@ -13,8 +13,14 @@ public class ProgressBarThread extends Thread {
     private final FileOutputStream fos;
     private final long totalDownloadBytes;
     private final int charAmt;
-    
-   
+
+
+    /**
+     * Progress Bar Constructor to initialise data members.
+     * @param fos Output stream for writing contents from the web to the local file.
+     * @param totalDownloadBytes Total size of the file in bytes.
+     * @param fileName Filename of the downloaded file.
+     */
     public ProgressBarThread(FileOutputStream fos, long totalDownloadBytes, String fileName) {
         this.charAmt = 30; // value to determine length of terminal progressbar
         this.downloading = true;
@@ -26,12 +32,22 @@ public class ProgressBarThread extends Thread {
         this.charPercent = (int)(this.totalDownloadBytes/charAmt);
 
     }
+
+    /**
+     * @param downloading true if the file is being downloaded and false if it's not.
+     */
     public void setDownloading(boolean downloading) {
         this.downloading = downloading;
         if (downloading) {
             System.out.println("Downloading " + fileName + " ...");
         }
     }
+
+    /**
+     * Generates progress bar.
+     * @param spinner icon
+     * @return String object containing the progress bar.
+     */
     private String generateProgressBar(String spinner) {
         float filled = downloadedBytes/charPercent;
         String a = new String(new char[(int)filled]).replace("\0", "=");
@@ -40,6 +56,11 @@ public class ProgressBarThread extends Thread {
         bar = bar.substring(0, charAmt/2-2) + String.format("%02d", (int)(downloadedBytes*100/totalDownloadBytes)) +"%" + bar.substring(charAmt/2+1);
         return "["+spinner+"]  "+fileName+"  (0KB)["+bar+"]("+convertBytes(totalDownloadBytes)+")  " + (float)downloadSpeed/1000000 + " MB/s      ";
     }
+
+    /**
+     * @param bytes file size in bytes.
+     * @return returns a String object containing the file size with proper units.
+     */
     private String convertBytes(long bytes) {
         String sizeWithUnit;
         if (bytes> 1000) {
@@ -57,6 +78,10 @@ public class ProgressBarThread extends Thread {
             return totalDownloadBytes+ " bytes";
         }
     }
+
+    /**
+     * Cleans up the resources.
+     */
     private void cleanup(){
         System.out.println("\r"+generateProgressBar("/"));
         if (downloadedBytes == totalDownloadBytes) {
@@ -68,6 +93,10 @@ public class ProgressBarThread extends Thread {
             Drifty_CLI.logger.log("ERROR", "Download failed!");
         }
     }
+
+    /**
+     * run method for progress bar.
+     */
     @Override
     public void run() {
         long initialMeasurement;
