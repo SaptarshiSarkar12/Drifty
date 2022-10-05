cce10b93931cf2e60eca6b68974f926e905139ef
Aryan Upadhyay <aryan.offc@gmail.com>
2022-10-05 16:50:59 +0530

    Added dark mode color scheme.

--- Website/script.js
+++ Website/script.js
@@ -1,19 +1,19 @@
-const options = {
-  bottom: "32px",
-  right: "32px",
-  left: "unset",
-  time: "0.5s",
-  mixColor: "#fff",
-  backgroundColor: "#fff",
-  buttonColorDark: "#100f2c",
-  buttonColorLight: "#fff",
-  saveInCookies: false,
-  label: "🌓",
-  autoMatchOsTheme: true,
-};
-
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
+// const options = {
+//   bottom: "32px",
+//   right: "32px",
+//   left: "unset",
+//   time: "0.5s",
+//   mixColor: "#fff",
+//   backgroundColor: "#fff",
+//   buttonColorDark: "#100f2c",
+//   buttonColorLight: "#fff",
+//   saveInCookies: false,
+//   label: "🌓",
+//   autoMatchOsTheme: true,
+// };
+//
+// const darkmode = new Darkmode(options);
+// darkmode.showWidget();
 // const swiper = new Swiper(".swiper", {
 //   loop: true,
 //   pagination: {
@@ -25,3 +25,27 @@ darkmode.showWidget();
 //     prevEl: ".swiper-button-prev",
 //   },
 // });
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('.btn-toggle');
+
+const enableDarkMode = () => {
+  document.body.classList.add('darkmode');
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  document.body.classList.remove('darkmode');
+  localStorage.setItem('darkMode', null);
+}
+
+if (darkMode === 'enabled')
+  enableDarkMode();
+
+darkModeToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('darkMode');
+  if (darkMode !== 'enabled')
+    enableDarkMode();
+  else
+    disableDarkMode();
+});
+
