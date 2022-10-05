88611d0ab9b276b380816882711e63410f8feeca
Aryan Upadhyay <aryan.offc@gmail.com>
2022-10-05 17:14:56 +0530

    removed commented code from script.js, possibly the video is working fine now!! 🙏

--- Website/script.js
+++ Website/script.js
@@ -1,30 +1,3 @@
-// const options = {
-//   bottom: "32px",
-//   right: "32px",
-//   left: "unset",
-//   time: "0.5s",
-//   mixColor: "#fff",
-//   backgroundColor: "#fff",
-//   buttonColorDark: "#100f2c",
-//   buttonColorLight: "#fff",
-//   saveInCookies: false,
-//   label: "🌓",
-//   autoMatchOsTheme: true,
-// };
-//
-// const darkmode = new Darkmode(options);
-// darkmode.showWidget();
-// const swiper = new Swiper(".swiper", {
-//   loop: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
 let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.querySelector('.btn-toggle');
 
