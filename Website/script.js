588802ea6909b18d3c4a414d846a9e462d7fc1a8
Waxer59 <hnboyano@gmail.com>
2022-10-05 21:25:25 +0200

    organize css, delete unused css id, fix darkmode toggle & added semantic html to the footer

--- Website/script.js
+++ Website/script.js
@@ -3,11 +3,13 @@ const darkModeToggle = document.querySelector('.btn-toggle');
 
 const enableDarkMode = () => {
   document.body.classList.add('darkmode');
+  darkModeToggle.checked = true; // Check only if the darkmode is on
   localStorage.setItem('darkMode', 'enabled');
 }
 
 const disableDarkMode = () => {
   document.body.classList.remove('darkmode');
+  darkModeToggle.checked = false;
   localStorage.setItem('darkMode', null);
 }
 
