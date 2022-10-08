7cbd8d7eb578ab385738c226ab55e95b532bbfea
Anurag-Bharati <anuragbharati26@gmail.com>
2022-10-08 20:29:35 +0545

    Added dynamic contributor fetch-n-apply functions. Rework on contrib section. Removed bootstrap to prevent conflicts w/ old css.

--- Website/script.js
+++ Website/script.js
@@ -1,3 +1,7 @@
+"use strict";
+
+const date = new Date();
+
 let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.querySelector('.btn-toggle');
 
@@ -5,7 +9,6 @@ const nav = document.querySelector(".nav-bar");
 const menu = document.getElementById("menu");
 const toggleBtn = document.querySelector("#menu>i");
 const miniNav = document.querySelector("nav");
-
 const links = document.querySelectorAll(".nav-links > li");
 
 const enableDarkMode = () => {
@@ -58,8 +61,54 @@ window.addEventListener("scroll", function () {
   }
   toggleBtn.classList.remove("fa-x");
   toggleBtn.classList.add("fa-bars");
-  }
-  
-  // END NAV ---------
+}
+// END NAV ---------
 
+// CONTRIB SECTION ---- 
+let currentHour = date.getHours();
+let contrib = localStorage.getItem('contrib');
+let requestedAt = localStorage.getItem("requestedAt");
+if (parseInt(requestedAt)>23) localStorage.removeItem("requestedAt");
+if (contrib == null ||  requestedAt == null) getContrib();
+else if (currentHour!=requestedAt) getContrib();
+function getContrib() {
+    fetch("https://api.github.com/repos/SaptarshiSarkar12/Drifty/contributors")
+    .then(x=>x.json())
+    .then((data) => {
+      contrib = data;
+      localStorage.setItem('contrib', JSON.stringify(data));
+      localStorage.setItem('requestedAt', date.getHours());
+  }).catch(err => console.log(err));
+}
+
+updateContrib();
+function updateContrib(){
+  try{
+    contrib = JSON.parse(contrib);
+  } catch (_) {
+  console.log(_);
+  }
+  contrib.forEach(x=> document
+    .getElementById("contributors")
+    .appendChild(generateDiv(x))); 
+}
+
+// x - user_object
+// returns - div with img
+function generateDiv(x) {
+  let img = document.createElement("img");
+  img.src = `${x["avatar_url"]}`;
+  img.style.height = "48px";
+  img.style.width = "48px";
+  let a = document.createElement("a");
+  a.href = `${x["html_url"]}`;
+  a.appendChild(img);
   
+  let div = document.createElement("div");
+  div.appendChild(a);
+    div.style.width = "48px";
+    div.style.height = "48px";
+    div.style.margin = "5px"
+    return div;
+}
+// END CONTRIB ---- 
