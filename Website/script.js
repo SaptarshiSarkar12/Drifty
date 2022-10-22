06369051b31e8fa8cf5f8bc4062cfe46dcea4d4b
Redet Getachew <redetg@gmail.com>
2022-10-22 23:06:28 +0300

    Identifies and adds latest label to latest release

--- Website/script.js
+++ Website/script.js
@@ -155,11 +155,22 @@ else if (currentHour - releasesRequestedAt >= 4)
   getReleases(); // get release info every 4 hours
 else updateReleases();
 
+// latest release could be defined as release with higher id;
+function setLatest(releases) {
+  let maxiId = Math.max(...releases.map((re) => re.id));
+  releases = releases.map((release) => {
+    release.isLatest = release.id == maxiId;
+    return release;
+  });
+  return releases;
+}
+
 function getReleases() {
   fetch("https://api.github.com/repos/SaptarshiSarkar12/Drifty/releases")
     .then((x) => x.json())
     .then((data) => {
       releases = data;
+      releases = setLatest(releases);
       localStorage.setItem("releases", JSON.stringify(data));
       localStorage.setItem("releasesRequestedAt", date.getHours());
       updateReleases();
@@ -187,7 +198,9 @@ function renderRelease(all, release) {
   );
   let assets = renderAssets(release.assets.reverse());
   return `${all} <div class="release">
-  <div style="text-align:center"><b>${release.name}</b></div>
+  <div style="text-align:center"><b>${release.name}</b> ${
+    release.isLatest ? "<b class='label'>Latest</b>" : ""
+  }</div>
   <div style="text-align:center">${new Date(release.published_at)} with <b>${
     release.totalCount
   } </b> Downloads</div>
