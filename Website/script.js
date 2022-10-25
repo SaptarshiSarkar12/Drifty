56a04b9685248eb4d06730e454d1bcf1e3ea12cc
Anant Nipunge <anantnipunge425@gmail.com>
2022-10-25 22:54:28 +0530

    footer updated according to dark and light mode

--- Website/script.js
+++ Website/script.js
@@ -246,4 +246,20 @@ function renderAssets(assets) {
     }
   }, "");
 }
+
+
+let mybutton = document.getElementById("mybtn")
+window.onscroll = function() {scrollFunction()}
+function scrollFunction(){
+  if(document.body.scrollTop > 650 || document.documentElement.scrollTop > 650){
+    mybutton.style.display = "block"
+  } else {
+    mybutton.style.display = "none"
+  }
+}
+function totop(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 // END RELEASE
