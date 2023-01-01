0c92c237e47142a5a59931a0abc9b53a68420c3f
bharathkalyans <sbharath35@gmail.com>
2023-01-01 14:32:37 +0530

    Extracted Hard Coded String values in FileDownloader.java and renamed variables to proper naming.

--- src/CheckDirectory.java
+++ src/CheckDirectory.java
@@ -4,25 +4,29 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static constants.DriftyConstants.*;
+
 /**
  * This class checks if a directory exists or not. if it doesn't, the directory is created.
  */
 class CheckDirectory {
     /**
      * This constructor creates the directory if it does not exist.
+     *
      * @param dir Name of the folder where the user wants to download the file.
      * @throws IOException when creating the directory fails.
      */
     CheckDirectory(String dir) throws IOException {
-        if (!(checkIfFolderExists(dir))){
+        if (!(checkIfFolderExists(dir))) {
             Path directory = FileSystems.getDefault().getPath(dir);
             Files.createDirectory(directory);
-            Drifty_CLI.logger.log("INFO", "Directory Created");
+            Drifty_CLI.logger.log(LOGGER_INFO, DIRECTORY_CREATED);
         }
     }
 
     /**
      * This function checks if a folder exists or not.
+     *
      * @param folderName Name of the folder where the user wants to download the file.
      * @return true if the folder exists and false if the folder is missing.
      */
@@ -34,8 +38,8 @@ class CheckDirectory {
                 found = true;
             }
         } catch (Exception e) {
-            System.out.println("Error while checking for directory !");
-            Drifty_CLI.logger.log("ERROR", "Error while checking for directory !");
+            System.out.println(ERROR_WHILE_CHECKING_FOR_DIRECTORY);
+            Drifty_CLI.logger.log(LOGGER_ERROR, ERROR_WHILE_CHECKING_FOR_DIRECTORY);
         }
         return found;
     }
