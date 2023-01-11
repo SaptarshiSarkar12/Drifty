1d98f6ac0799d9befb2da9c79b7e50ae39fb0ada
Saptarshi <saptarshi.programmer@gmail.com>
2023-01-12 02:42:04 +0530

    Updated
    Signed-off-by: Saptarshi <saptarshi.programmer@gmail.com>

--- src/GUI/Drifty_GUI.java
+++ src/GUI/Drifty_GUI.java
@@ -1,9 +1,11 @@
 package GUI;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class Drifty_GUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
