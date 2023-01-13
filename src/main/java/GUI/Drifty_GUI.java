2594ab66c6a0dda716390321b9229ed906985fcc
Saptarshi <saptarshi.programmer@gmail.com>
2023-01-13 09:51:24 +0530

    Added main GUI file and added support for inputs
    Signed-off-by: Saptarshi <saptarshi.programmer@gmail.com>

--- src/main/java/GUI/Drifty_GUI.java
+++ src/main/java/GUI/Drifty_GUI.java
@@ -1,12 +1,11 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -23,6 +22,7 @@ public class Drifty_GUI extends Application {
     static MenuBar menuBar;
     static Text drifty;
     static VBox input;
+    static String linkToFile;
     @Override
     public void start(Stage mainWindow) {
         driftyInitialWindow = mainWindow;
@@ -31,12 +31,13 @@ public class Drifty_GUI extends Application {
 
         root.getChildren().addAll(menuBar, drifty, input);
         mainWindow.setScene(mainScene);
+        mainWindow.sizeToScene();
         mainWindow.show();
     }
 
     private static void takeInputs() {
         input = new VBox();
-        input.setSpacing(12);
+        input.setSpacing(20);
 
         HBox link = new HBox();
         Text linkText = new Text("Link : ");
@@ -46,6 +47,23 @@ public class Drifty_GUI extends Application {
         linkInput.setPrefColumnCount(60);
         link.getChildren().addAll(linkText, linkInput);
 
+        Button validateLink = new Button("Validate Link");
+        EventHandler<ActionEvent> linkEnter = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                linkToFile = String.valueOf(linkInput.getCharacters());
+                Text linkValidOrNot = new Text();
+                linkValidOrNot.setFont(Font.font("Algerian", FontWeight.MEDIUM, 12));
+                if (CLI.utility.DriftyUtility.isURLValid(linkToFile)){
+                    linkValidOrNot.setText("Link is Valid");
+                } else {
+                    linkValidOrNot.setText("Invalid link! Please enter again!");
+                }
+                input.getChildren().add(linkValidOrNot);
+            }
+        };
+        validateLink.setOnAction(linkEnter);
+
         HBox directory = new HBox();
         Text directoryText = new Text("Directory : ");
         directoryText.setFont(Font.font("Arial", 20));
@@ -54,7 +72,7 @@ public class Drifty_GUI extends Application {
         directoryInput.setPrefColumnCount(58);
         directory.getChildren().addAll(directoryText, directoryInput);
 
-        input.getChildren().addAll(link, directory);
+        input.getChildren().addAll(link, validateLink, directory);
     }
 
     public static void main(String[] args) {
@@ -64,7 +82,7 @@ public class Drifty_GUI extends Application {
     private static void initializeScreen(){
         driftyInitialWindow.setTitle("Drifty GUI [Under Development]");
         driftyInitialWindow.setMaximized(true);
-        root.setSpacing(10);
+        root.setSpacing(15);
 
         menuBar = new MenuBar();
         Menu menu = new Menu("Menu");
