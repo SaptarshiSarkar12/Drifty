c9d3d3b8f6a1fa61900ada39229e85715bde71ef
Naveen p <naveenoffl2002@gmail.com>
2023-02-19 18:37:32 +0530

    Setting up the environment

---
+++ drifty-website/components/Contribute.js
@@ -0,0 +1,7 @@
+export default function Contribute(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
---
+++ drifty-website/pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+import Header from '../components/Header'
+import Contribute from '../components/Contribute'
+import Demo from '../components/Demo'
+import Download from '../components/Download'
+import Footer from '../components/Footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Drifty</title>
+        <meta name="description" content="Interactive File Downloader System" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+        <link rel="manifest" href="favicons/site.webmanifest" />
+      </Head>
+      <Header />
+      <Contribute/>
+      {/* <Download />
+      <Demo />
+      <Footer /> */}
+    </>
+  )
+}
