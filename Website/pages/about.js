8b94d0867a39b7b9a2d5b4ad2d6c7d92afaa35fb
Naveen p <naveenoffl2002@gmail.com>
2023-03-26 13:09:01 +0530

    Static Generation

--- Website/pages/about.js
+++ Website/pages/about.js
@@ -1,5 +1,5 @@
-import Footer from "@/components/Footer"
-import Header from "@/components/Header"
+import Footer from "/components/Footer"
+import Header from "/components/Header"
 import Image from "next/image"
 export default function about(){
     return(
--- Website/pages/contact.js
+++ Website/pages/contact.js
@@ -1,5 +1,5 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Footer from "/components/Footer";
+import Header from "/components/Header";
 
 export default function contact(){
     return(
