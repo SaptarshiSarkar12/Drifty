4dfedf1b525cd4b12902c5194d98f51ac6cbd95c
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2023-04-24 12:27:36 +0530

    Update next.config.js

--- Website/next.config.js
+++ Website/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  assetPrefix: '/SaptarshiSarkar12/',
-  basePath: '/SaptarshiSarkar12/',
+  assetPrefix: '/SaptarshiSarkar12',
+  basePath: '/SaptarshiSarkar12',
   images: {
     domains: ['avatars.githubusercontent.com','camo.githubusercontent.com','cdn.jsdelivr.net'],
     dangerouslyAllowSVG: true,
