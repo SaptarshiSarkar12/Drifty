7d2123229319e936bceb43f2efd4f47149c5275f
Saptarshi <saptarshi.programmer@gmail.com>
2023-04-24 13:42:22 +0530

    Updated
    Signed-off-by: Saptarshi <saptarshi.programmer@gmail.com>

--- Website/components/Demo.jsx
+++ Website/components/Demo.jsx
@@ -4,10 +4,10 @@ export default function Demo(){
         <h1 className="text-center font-extrabold text-4xl pt-6">Demonstration of Drifty</h1>
         <p className="text-center text-black text-2xl py-3">Here is a quick demo of using Drifty.</p>
         <div className="flex justify-center">
-        <video  width="80%" autoPlay loop>
-        <source src="Resources/Usage.webm" type='video/webm'/>
-        Your browser does not support the video tag
-        </video>
+        	<video  width="80%" autoPlay loop>
+        		<source src="Resources/Usage.webm" type='video/webm'/>
+        			Your browser does not support the video tag
+        	</video>
         </div>
         </div>
     )
