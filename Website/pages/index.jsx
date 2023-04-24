3beed7a349608da82e886c1a4f828b16af960f44
Saptarshi <saptarshi.programmer@gmail.com>
2023-04-24 15:34:30 +0530

    Updated
    Signed-off-by: Saptarshi <saptarshi.programmer@gmail.com>

--- Website/pages/index.jsx
+++ Website/pages/index.jsx
@@ -16,6 +16,7 @@ export default function Home(props) {
         <title>Drifty</title>
         <meta name="description" content="Interactive File Downloader System" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" type="image/x-icon" href="favicons/favicon.ico">
         <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
