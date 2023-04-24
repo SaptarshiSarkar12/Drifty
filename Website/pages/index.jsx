af0fc4e25d5225f02d88af6d4fdc05d62ec39b0c
Saptarshi <saptarshi.programmer@gmail.com>
2023-04-24 15:18:06 +0530

    Updated
    Signed-off-by: Saptarshi <saptarshi.programmer@gmail.com>

--- Website/pages/index.jsx
+++ Website/pages/index.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Header from '../components/Header'
 import Contribute from '../components/Contribute'
@@ -11,7 +12,15 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home(props) {
   return (
     <>
-      <Head />
+      <Head>
+        <title>Drifty</title>
+        <meta name="description" content="Interactive File Downloader System" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+        <link rel="manifest" href="favicons/site.webmanifest" />
+      </Head>
       <Header props={"bg-top"} />
       <MainSection />
       <Contribute props={props.contribs}/>
