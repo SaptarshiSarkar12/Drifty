5e264c5d7d264845722f8325ee5852253b745451
Saptarshi <saptarshi.programmer@gmail.com>
2023-04-24 14:11:30 +0530

    Updated
    Signed-off-by: Saptarshi <saptarshi.programmer@gmail.com>

--- Website/pages/_app.jsx
+++ Website/pages/_app.jsx
@@ -1,5 +1,11 @@
 import '/styles/globals.css'
 
+<Head>
+<link rel="shortcut icon" href="/favicons/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
+</Head>
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
