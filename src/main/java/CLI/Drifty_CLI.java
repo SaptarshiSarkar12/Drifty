d8b161d2eb5730399e9ba00192832e013c8afd09
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2023-06-16 12:27:05 +0530

    feat(App): Added more features to Batch Downloading through YAML/YML files

--- src/main/java/CLI/Drifty_CLI.java
+++ src/main/java/CLI/Drifty_CLI.java
@@ -35,12 +35,21 @@ public class Drifty_CLI {
      * Boolean value which determines if the given link is an YouTube video URL or not
      */
     protected static boolean isYoutubeURL;
+    /**
+     * Message broker instance which helps to send messages to the output stream
+     */
     private static MessageBroker messageBroker;
     private static String link;
     private static Utility utility;
     private static String directory;
     private static String fileName = null;
+    /**
+     * Boolean value which determines if batch downloading is triggered by the user
+     */
     private static boolean batchDownloading;
+    /**
+     * The path of the YAML/YML data file which contains the required data, in String format
+     */
     private static String batchDownloadingFile;
 
     /**
@@ -80,23 +89,7 @@ public class Drifty_CLI {
                 if (!isYoutubeURL) {
                     fileName = utility.findFilenameInLink(link);
                 }
-                if ((fileName == null || (fileName.length() == 0)) && (!isYoutubeURL)) {
-                    System.out.print(ENTER_FILE_NAME_WITH_EXTENSION);
-                    fileName = SC.nextLine();
-                } else {
-                    if (isYoutubeURL) {
-                        System.out.print("Do you like to use the video title as the filename? (Enter Y for yes and N for no) : ");
-                    } else {
-                        System.out.print(RENAME_FILE);
-                    }
-                    SC.nextLine(); // To remove 'whitespace' from input buffer.
-                    String choiceString = SC.nextLine();
-                    boolean choice = utility.yesNoValidation(choiceString, RENAME_FILE);
-                    if (!choice) {
-                        System.out.print(ENTER_FILE_NAME_WITH_EXTENSION);
-                        fileName = SC.nextLine();
-                    }
-                }
+                takeFileNameInputIfNull();
                 downloadsFolder = location;
                 if (downloadsFolder == null) {
                     downloadsFolder = utility.saveToDefault();
@@ -125,8 +118,12 @@ public class Drifty_CLI {
                     System.out.print("Enter the path to the YAML data file : ");
                     batchDownloadingFile = SC.next();
                     SC.nextLine();
-                    batchDownloader();
-                    break;
+                    if (!(batchDownloadingFile.endsWith(".yml") || batchDownloadingFile.endsWith(".yaml"))){
+                        messageBroker.sendMessage("The given file should be a YAML file!", LOGGER_ERROR, "only log");
+                    } else {
+                        batchDownloader();
+                        break;
+                    }
                 } else if (choice == 2) {
                     batchDownloading = false;
                     break;
@@ -180,24 +177,87 @@ public class Drifty_CLI {
     private static void batchDownloader() {
         Yaml yamlInstance = new Yaml();
         try {
+            messageBroker.sendMessage("Trying to load YAML data file (" + batchDownloadingFile + ") ...", LOGGER_INFO, "only log");
             InputStreamReader yamlDataFile = new InputStreamReader(new FileInputStream(batchDownloadingFile));
             Map<String, List<String>> data = yamlInstance.load(yamlDataFile);
-            for (int i = 0; i < data.size() - 1; i++) {
-                link = data.get("links").get(i);
-                fileName = data.get("fileNames").get(i);
-                if (data.containsKey("directory")) {
-                    directory = data.get("directory").get(0);
+            messageBroker.sendMessage("YAML data file (" + batchDownloadingFile + ") loaded successfully", LOGGER_INFO, "only log");
+            int numberOfLinks = data.get("links").size();
+            int numberOfFileNames = data.get("fileNames").size();
+            int numberOfDirectories = 0;
+            if (data.containsKey("directory")) {
+                numberOfDirectories = 1;
+                if (data.get("directory").get(0).length() == 0) {
+                    directory = ".";
                 } else {
-                    directory = data.get("directories").get(i);
+                    directory = data.get("directory").get(0);
+                }
+            } else if (data.containsKey("directories")) {
+                numberOfDirectories = data.get("directories").size();
+            }
+            String linkMessage;
+            if (numberOfLinks == 1) {
+                linkMessage = numberOfLinks + " link";
+            } else {
+                linkMessage = numberOfLinks + " links";
+            }
+            String directoryMessage;
+            if (numberOfDirectories == 1) {
+                directoryMessage = numberOfDirectories + " directory";
+            } else {
+                directoryMessage = numberOfDirectories + " directories";
+            }
+            String fileNameMessage;
+            if (numberOfFileNames == 1) {
+                fileNameMessage = numberOfFileNames + " filename";
+            } else {
+                fileNameMessage = numberOfFileNames + " filenames";
+            }
+            messageBroker.sendMessage("You have provided\n\t" + linkMessage + "\n\t" + directoryMessage + "\n\t" + fileNameMessage, LOGGER_INFO, "only log");
+            for (int i = 0; i < numberOfLinks; i++) {
+                link = data.get("links").get(i);
+                try {
+                    fileName = data.get("fileNames").get(i);
+                } catch (Exception e) {
+                    fileName = utility.findFilenameInLink(link);
+                    takeFileNameInputIfNull();
                 }
                 if (directory.equals(".")){
                     directory = utility.saveToDefault();
+                } else if (directory.equals("")) {
+                    try {
+                        directory = data.get("directories").get(i);
+                    } catch (Exception e) {
+                        directory = ".";
+                    }
                 }
                 Drifty backend = new Drifty(link, directory, fileName, System.out);
                 backend.start();
             }
         } catch (FileNotFoundException e) {
-            messageBroker.sendMessage("YAML Data File not found ! " + e.getMessage(), LOGGER_ERROR, "download");
+            messageBroker.sendMessage("YAML Data file (" + batchDownloadingFile + ") not found ! " + e.getMessage(), LOGGER_ERROR, "download");
+        }
+    }
+
+    /**
+     * Takes the filename as input from the user interactively if the system cannot find the filename i.e. if it is null
+     */
+    private static void takeFileNameInputIfNull() {
+        if ((fileName == null || (fileName.length() == 0)) && (!isYoutubeURL)) {
+            System.out.print(ENTER_FILE_NAME_WITH_EXTENSION);
+            fileName = SC.nextLine();
+        } else {
+            if (isYoutubeURL) {
+                System.out.print("Do you like to use the video title as the filename? (Enter Y for yes and N for no) : ");
+            } else {
+                System.out.print(RENAME_FILE);
+            }
+            SC.nextLine(); // To remove 'whitespace' from input buffer.
+            String choiceString = SC.nextLine();
+            boolean choice = utility.yesNoValidation(choiceString, RENAME_FILE);
+            if (!choice) {
+                System.out.print(ENTER_FILE_NAME_WITH_EXTENSION);
+                fileName = SC.nextLine();
+            }
         }
     }
 }
