68b2bfa8b454657bacf24bf48801e451ba2170e4
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2023-07-20 11:35:53 +0530

    chore: Added # in home and Drifty icon in website home page

--- Website/app/Header.js
+++ Website/app/Header.js
@@ -13,7 +13,7 @@ function MobileNav({ open, setOpen }) {
     return (
         <div className={`absolute z-10 top-0 -mt-7 pt-2 left-0 h-screen w-screen bg-top transform ${!open && "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md`}>
             <div className="flex items-start justify-center filter bg-top h-30"> {/*logo container*/}
-                <Link className="text-xl font-semibold" href="/"><Image src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/app/icon.png" width={60} height={60} alt="Logo of Drifty"/></Link>
+                <Link className="text-xl font-semibold" href="/#"><Image src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/app/icon.png" width={60} height={60} alt="Logo of Drifty"/></Link>
             </div>
             <div className="grid grid-rows-3 justify-items-center bg-top">
                 <NavLink to="/about" cn="text-xl font-normal my-4">
@@ -49,7 +49,7 @@ export default function Header({props}) {
             <nav className="flex filter drop-shadow-md  px-4 py-4 h-20 items-center">
                 <MobileNav open={open} setOpen={setOpen} />
                 <div className="w-4/12 flex items-center lg:pl-20">
-                    <Link className="font-semibold" href="/"><Image className=" w-12" src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/app/icon.png" alt="Logo Of Drifty" width={192} height={192}/></Link>
+                    <Link className="font-semibold" href="/#"><Image className=" w-12" src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/app/icon.png" alt="Logo Of Drifty" width={192} height={192}/></Link>
                 </div>
                 <div className="w-9/12 flex justify-end items-center">
                     <div className="z-40 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => { setOpen(!open) }}>
@@ -59,7 +59,7 @@ export default function Header({props}) {
                         <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
                     </div>
                     <div className="hidden md:flex mr-20">
-                    	<NavLink to="/" cn="mx-4">
+                    	<NavLink to="/#" cn="mx-4">
                     		<button className="p-2 rounded-full shadow-lg bg-btn-color hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">HOME</button>
             		</NavLink>
                     	<NavLink to="/about" cn="mx-4">
