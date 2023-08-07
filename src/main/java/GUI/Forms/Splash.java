8e83e467839312e0acdc0b9a99cdd1151f371f0e
Michael Sims <sims.mike@gmail.com>
2023-08-06 22:29:55 -0700

    Added splash screen

--- src/main/java/GUI/Forms/Splash.java
+++ src/main/java/GUI/Forms/Splash.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -35,8 +36,10 @@ public class Splash extends Application {
         ivSplash.setPreserveRatio(true);
         ivSplash.setFitWidth(fitWidth);
         Pane pane = new Pane();
+        pane.setStyle("-fx-background-color: transparent");
         pane.getChildren().add(ivSplash);
         Scene scene = new Scene(pane);
+        scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.setAlwaysOnTop(true);
