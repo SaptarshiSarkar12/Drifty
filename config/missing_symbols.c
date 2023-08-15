8b993050aaa4792d87631fdd0fb000c6f4d7cf6b
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2023-08-15 17:15:46 +0530

    ci: Added C object build task for the missing libraries for all OS

---
+++ config/missing_symbols.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+void Java_jdk_net_WindowsSocketOptions_getIpDontFragment0(){}
+void Java_jdk_net_WindowsSocketOptions_setIpDontFragment0(){}
+void TransmitFile(){}
+void Java_com_sun_management_internal_OperatingSystemImpl_initialize0(){}
+void _Java_com_sun_management_internal_OperatingSystemImpl_initialize0(){}
