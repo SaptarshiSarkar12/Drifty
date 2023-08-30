e6fdde420be5df2a8fee22b2df085a60568a4baf
Michael Sims <sims.mike@gmail.com>
2023-08-30 05:59:27 -0700

    Cleaned up MessageBroker and added method logOnly

--- src/main/java/Utils/MessageBroker.java
+++ src/main/java/Utils/MessageBroker.java
@@ -8,15 +8,12 @@ import javafx.scene.paint.Color;
 
 import java.io.PrintStream;
 
+import static Enums.MessageCategory.LOG;
+import static javafx.scene.paint.Color.*;
+
 public class MessageBroker {
     Logger logger;
     PrintStream output;
-    public static final Color GREEN = Color.rgb(0, 255, 0);
-    public static final Color TEAL = Color.rgb(0, 255, 255);
-    public static final Color RED = Color.rgb(157, 0, 0);
-    public static final Color PURPLE = Color.rgb(125, 0, 75);
-    public static final Color BLACK = Color.rgb(0, 0, 0);
-    public static final Color YELLOW = Color.rgb(255, 255, 0);
 
     public MessageBroker(PrintStream consoleOutput) {
         output = consoleOutput;
@@ -27,32 +24,32 @@ public class MessageBroker {
         logger = Logger.getInstance();
     }
 
+    public void logOnly(String message, MessageType messageType) {
+        output.println(message);
+        logger.log(messageType, message);
+    }
+
     public void sendMessage(String message, MessageType messageType, MessageCategory messageCategory) {
         if (Mode.isCLI()) {
-            if (!messageCategory.equals(MessageCategory.LOG)) {
+            if (!messageCategory.equals(LOG)) {
                 output.println(message);
             }
             logger.log(messageType, message);
-        } else if (Mode.isGUI()) {
+        }
+        else if (Mode.isGUI()) {
             if (!message.isEmpty()) {
                 logger.log(messageType, message);
             }
-            Color color;
-            if (messageType.equals(MessageType.ERROR)) {
-                color = RED;
-            } else if (messageType.equals(MessageType.INFO)) {
-                color = GREEN;
-            } else {
-                color = YELLOW;
-            }
-            if (messageCategory.equals(MessageCategory.LINK)) {
-                FormLogic.INSTANCE.setLinkOutput(color, message);
-            } else if (messageCategory.equals(MessageCategory.FILENAME)) {
-                FormLogic.INSTANCE.setFilenameOutput(color, message);
-            } else if (messageCategory.equals(MessageCategory.DIRECTORY)) {
-                FormLogic.INSTANCE.setDirOutput(color, message);
-            } else if (messageCategory.equals(MessageCategory.DOWNLOAD)) {
-                FormLogic.INSTANCE.setDownloadOutput(color, message);
+            Color color = switch (messageType) {
+                case ERROR -> RED;
+                case INFO -> GREEN;
+                default -> YELLOW;
+            };
+            switch (messageCategory) {
+                case LINK -> FormLogic.INSTANCE.setLinkOutput(color, message);
+                case FILENAME -> FormLogic.INSTANCE.setFilenameOutput(color, message);
+                case DIRECTORY -> FormLogic.INSTANCE.setDirOutput(color, message);
+                case DOWNLOAD -> FormLogic.INSTANCE.setDownloadOutput(color, message);
             }
         }
     }
