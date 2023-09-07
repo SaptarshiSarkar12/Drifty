c110592eb396c8b186895ec46e7681c7666d269d
Michael Sims <sims.mike@gmail.com>
2023-09-07 02:30:07 -0700

    Changed file rename on duplicate to add a sequential integer to name instead of random text. Fixed issues with link TextField so that all pasted text works every time.

--- src/main/java/GUI/Support/JobHistory.java
+++ src/main/java/GUI/Support/JobHistory.java
@@ -11,7 +11,6 @@ public class JobHistory {
 
     public JobHistory() {
         this.jobList = new ConcurrentLinkedDeque<>();
-        this.jobList.add(new Job("Template","JobHistory","Pattern", false));
     }
 
     private ConcurrentLinkedDeque<Job> jobList;
