fddc11b24f1fd54fbed718602ef73dee34651413
Akın Bilgin <108457883+akinpinkman@users.noreply.github.com>
2023-10-02 13:22:17 +0300

    feat: New redesigned hero section of the website
    * Hero section improvements
    
    * Fixed the scrollbar and navbar color transition
    
    * Small changes to responsiveness
    
    * Add socials for mobile nav
    
    * Logo added to navbar
    
    * Change the logo placement
    
    * Logo aligned to the left
    
    * Small fix for mobile navbar
    
    * Logo added to mobile navbar
    
    * Padding adjustments
    
    * fix: Changed discord and github icons and enlarged the navbar texts
    
    ---------
    
    Co-authored-by: Saptarshi Sarkar <saptarshi.programmer@gmail.com>

--- Website/app/MainSection.js
+++ Website/app/MainSection.js
@@ -1,34 +1,84 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function MainSection() {
-    return (
-        <div className="flex flex-row-1 bg-top space-evenly lg:justify-start xs:justify-start select-none">
-            <div className="lg:flex lg:w-1/2">
-                <div className=" lg:flex lg:flex-col  text-white py-20 px-6 md:text" >
-                    <h1 className="text-8xl lg:ml-20 font-extrabold text-white  mt-0 mb-6">Drifty</h1>
-                    <p className="text-2xl  lg:ml-20 font-sans mb-8 ">
-                        Drifty is an Open-Source interactive File Downloader system built
-                        with Java. It takes the link of the file to be downloaded and
-                        downloads it in the appropriate folder with the appropriate filename
-                        retrieved from the link. It can download files from platforms including
-                        YouTube, Instagram, and many more.
-                    </p>
-                    <div className="flex space-x-3 justify-start lg:ml-20 lg:mt-3 lg:mb-16">
-                        <Link className="inline-block px-6 py-3.5 w-30 h-14 bg-gradient-to-r from-blue-800 via-blue-600 to-blue-800 text-white font-medium text-lg leading-tight rounded-lg shadow-md  hover:from-pink-500 hover:to-yellow-500 hover:transition ease-in-out duration-300 delay-100 hover:-translate-y-1 hover:scale-110 hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-400 active:shadow-lg transition" href="/#contribute" role="button">More Info</Link>
-                        <Link className="inline-block px-6 py-3.5 w-30 h-14 bg-gradient-to-r from-blue-800 via-blue-600 to-blue-800 text-white font-medium text-lg leading-tight rounded-lg shadow-md hover:from-pink-500 hover:to-yellow-500 hover:transition ease-in-out duration-300 delay-100 hover:-translate-y-1 hover:scale-110 hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-400 active:shadow-lg transition" href="/#demo" role="button">Demo</Link>
-                    </div>
-                </div>
-            </div>
-            <div className="xs:hidden flex-col-1 lg:block text-center text-gray-800 py-20 px-6 img-layer w-1/2" id="ImgLayer">
-                <Image width={500} height={0} className="absolute animate-img" src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/public/Layers/2.webp" alt="..." />
-                <Image width={500} height={0} className="absolute animate-img hover:bg-black" src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/public/Layers/3.webp" alt="..." />
-                <Image width={500} height={0} className="absolute animate-img" src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/public/Layers/4.webp" alt="..." />
-                <Image width={500} height={0} className="absolute animate-img" src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/public/Layers/5.webp" alt="..." />
-                <Image width={500} height={0} className="absolute animate-img" src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/public/Layers/6.webp" alt="..." />
-                <Image width={500} height={0} className="absolute animate-img" src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/public/Layers/7.webp" alt="..." />
-            </div>
-        </div>
+  return (
+    <div className="flex flex-col bg-top space-y-6 lg:flex-row lg:space-y-0 lg:space-evenly py-[2rem] lg:justify-start xs:justify-start select-none">
+      <div className="lg:w-1/2">
+        <div className="lg:flex lg:flex-col text-white px-4 py-4 sm:pl-[5rem] md:text">
+          <div className="flex lg:ml-4 lg:items-center border-b-4 pb-3 gap-2 ">
+            <h1 className="lg:text-8xl xs:text-7xl font-bold">Drifty</h1>
+          </div>
+          <p className="text-lg lg:text-2xl lg:ml-4 md:pr-20 font-sans my-4 xs:pb-2">
+            Drifty is an Open-Source interactive File Downloader system built
+            with Java. It takes the link of the file to be downloaded and
+            downloads it in the appropriate folder with the appropriate filename
+            retrieved from the link. It can download files from platforms
+            including YouTube, Instagram, and many more.
+          </p>
 
-    )
+          <div className="flex space-x-2 justify-start lg:ml-4 lg:mt-2 lg:mb-8">
+            <Link
+              className="border-2 px-3 py-2 text-white rounded hover:border-[#191B33] hover:bg-[#191B33] hover:scale-105 duration-500 text-sm lg:text-base"
+              href={"/download"}
+              role="button"
+            >
+              Download
+            </Link>
+            <Link
+              className="border-2 px-3 py-2 text-white rounded hover:border-[#191B33] hover:bg-[#191B33] hover:scale-105 duration-500 text-sm lg:text-base"
+              href={"/#demo"}
+              role="button"
+            >
+              View Demo
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="xs:hidden mx-auto lg:block text-center text-gray-800 py-4 px-2 img-layer w-1/2">
+        <Image
+          width={450}
+          height={0}
+          className="absolute animate-img"
+          src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/public/Layers/2.webp"
+          alt="..."
+        />
+        <Image
+          width={450}
+          height={0}
+          className="absolute animate-img hover:bg-black"
+          src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/public/Layers/3.webp"
+          alt="..."
+        />
+        <Image
+          width={450}
+          height={0}
+          className="absolute animate-img"
+          src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/public/Layers/4.webp"
+          alt="..."
+        />
+        <Image
+          width={450}
+          height={0}
+          className="absolute animate-img"
+          src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/public/Layers/5.webp"
+          alt="..."
+        />
+        <Image
+          width={450}
+          height={0}
+          className="absolute animate-img"
+          src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/public/Layers/6.webp"
+          alt="..."
+        />
+        <Image
+          width={450}
+          height={0}
+          className="absolute animate-img"
+          src="https://cdn.jsdelivr.net/gh/SaptarshiSarkar12/Drifty@master/Website/public/Layers/7.webp"
+          alt="..."
+        />
+      </div>
+    </div>
+  );
 }
--- Website/app/download/page.js
+++ Website/app/download/page.js
@@ -3,34 +3,37 @@ import Header from "../Header";
 import Footer from "../Footer";
 
 export const metadata = {
-    title: "Download",
-    description: 'Download Drifty',
-    themeColor: [
-        { media: '(prefers-color-scheme: dark)', color: '#0000cd' },
-        { media: '(prefers-color-scheme: light)', color: '#26a3f1' },
-    ],
-    viewport: {
-      width: 'device-width',
-      initialScale: 1,
-    },
-}
+  title: "Download",
+  description: "Download Drifty",
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#0000cd" },
+    { media: "(prefers-color-scheme: light)", color: "#26a3f1" },
+  ],
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+};
 
 export default async function download() {
-    const data = await getData()
-    return(
-        <>
-           <Header props={"bg-top"}/>
-           <Releases className="bg-about" props={data} />
-           <Footer />
-        </>
-    )    
+  const data = await getData();
+  return (
+    <>
+      <Header props={"bg-top"} />
+      <Releases className="bg-about" props={data} />
+      <Footer />
+    </>
+  );
 }
 
-export async function getData(){
-    const res = await fetch('https://api.github.com/repos/SaptarshiSarkar12/Drifty/releases', {method: 'GET'})
-    const release = await res.json();
-    return {
-      release,
-      revalidate:3600
-    }
+export async function getData() {
+  const res = await fetch(
+    "https://api.github.com/repos/SaptarshiSarkar12/Drifty/releases",
+    { method: "GET" }
+  );
+  const release = await res.json();
+  return {
+    release,
+    revalidate: 3600,
+  };
 }
