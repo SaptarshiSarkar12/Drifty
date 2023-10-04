839be7edecb5786f72dbbbe6ccb512b74957215b
Suryansh Soni <surisoni10@gmail.com>
2023-10-04 17:52:32 +0530

    Added About window for Drifty GUI

--- src/main/java/GUI/Forms/Main.java
+++ src/main/java/GUI/Forms/Main.java
@@ -6,14 +6,18 @@ import Utils.Environment;
 import Utils.MessageBroker;
 import Utils.Utility;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.Clipboard;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import static Utils.DriftyConstants.DRIFTY_WEBSITE_URL;
@@ -48,7 +52,7 @@ public class Main extends Application {
     private void createScene() {
         AnchorPane ap = new AnchorPane();
         MainGridPane gridPane = new MainGridPane();
-        MenuBar menu = menuBar(getMenuItemsOfMenu(), getEditMenu(), getWindowMenu(), getHelpMenu(), getAboutMenu());
+        MenuBar menu = menuBar(getMenuItemsOfMenu(), getEditMenu(), getWindowMenu(), getHelpMenu());
         ap.getChildren().add(gridPane);
         ap.getChildren().add(menu);
         placeControl(gridPane, 40, 40, 40, 40);
@@ -123,12 +127,54 @@ public class Main extends Application {
         MenuItem bug = new MenuItem("Report a Bug");
         MenuItem securityVulnerability = new MenuItem("Report a Security Vulnerability");
         MenuItem feature = new MenuItem("Suggest a Feature");
+        MenuItem aboutDrifty = new MenuItem("About Drifty");
         contactUs.setOnAction(e -> openWebsite("https://saptarshisarkar12.github.io/Drifty/contact.html"));
         contribute.setOnAction(e -> openWebsite("https://github.com/SaptarshiSarkar12/Drifty"));
         bug.setOnAction(e -> openWebsite("https://github.com/SaptarshiSarkar12/Drifty/issues/new?assignees=&labels=bug%2CApp&template=Bug-for-application.yaml&title=%5BBUG%5D+"));
         securityVulnerability.setOnAction(e -> openWebsite("https://github.com/SaptarshiSarkar12/Drifty/security/advisories/new"));
         feature.setOnAction(e -> openWebsite("https://github.com/SaptarshiSarkar12/Drifty/issues/new?assignees=&labels=enhancement%2CApp&template=feature-request-application.yaml&title=%5BFEATURE%5D+"));
-        menu.getItems().setAll(contactUs, contribute, bug, securityVulnerability, feature);
+        aboutDrifty.setOnAction(event -> {
+            Stage aboutStage = Constants.getStage("About Drifty", false);
+            VBox root = new VBox(10);
+            root.setPadding(new Insets(10));
+            root.setAlignment(Pos.TOP_CENTER);
+            root.setBackground(new Background(
+                    new BackgroundFill(
+                            new LinearGradient(
+                                    0, 0, 0, 1, true,
+                                    CycleMethod.NO_CYCLE,
+                                    new Stop(0, Color.web("#0061ff")),
+                                    new Stop(1, Color.web("#60efff"))),
+                            CornerRadii.EMPTY, Insets.EMPTY
+                    )
+            ));
+            ImageView logo = new ImageView(String.valueOf(Constants.class.getResource("/GUI/Splash.png")));
+            logo.setFitWidth(Screen.getPrimary().getBounds().getWidth()*.2);
+            logo.setFitHeight(Screen.getPrimary().getBounds().getHeight()*.2);
+            logo.setPreserveRatio(true);
+            Label nameLabel = new Label("An Open-Source Interactive File Downloader System");
+            nameLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+            Label versionLabel = new Label(DriftyConstants.VERSION_NUMBER);
+            versionLabel.setFont(Font.font("Arial",FontWeight.BOLD, 20));
+            versionLabel.setTextFill(Color.web("#FFFFFF"));
+            Hyperlink websiteLink = new Hyperlink("Website");
+            websiteLink.setFont(Font.font("Arial",FontWeight.BOLD, 18));
+            websiteLink.setTextFill(Color.web("#1211EA"));
+            websiteLink.setOnAction(e -> openWebsite("https://saptarshisarkar12.github.io/Drifty"));
+            Hyperlink discordLink = new Hyperlink("Join Discord");
+            discordLink.setFont(Font.font("Arial",FontWeight.BOLD, 18));
+            discordLink.setTextFill(Color.web("#5B11EA"));
+            discordLink.setOnAction(e -> openWebsite("https://discord.gg/YVGpXtNM"));
+            Hyperlink githubLink = new Hyperlink("Contribute to Drifty");
+            githubLink.setFont(Font.font("Arial",FontWeight.BOLD, 18));
+            githubLink.setTextFill(Color.BLACK);
+            githubLink.setOnAction(e -> openWebsite("https://github.com/SaptarshiSarkar12/Drifty"));
+            root.getChildren().addAll(logo, nameLabel, versionLabel, websiteLink, discordLink, githubLink);
+            Scene aboutScene = new Scene(root, Screen.getPrimary().getBounds().getWidth() * .5, Screen.getPrimary().getBounds().getHeight() * .5);
+            aboutStage.setScene(aboutScene);
+            aboutStage.show();
+        });
+        menu.getItems().setAll(contactUs, contribute, bug, securityVulnerability, feature, aboutDrifty);
         return menu;
     }
 
@@ -142,21 +188,6 @@ public class Main extends Application {
             }
         });
         menu.getItems().addAll(wipeHistory);
-        return menu;
-    }
-
-    private  Menu getAboutMenu(){
-        Menu menu = new Menu("About");
-        MenuItem aboutDrifty = new MenuItem("About Drifty");
-        aboutDrifty.setOnAction(event -> {
-            Stage aboutStage = Constants.getStage("Drifty GUI", false);
-            AnchorPane ap = new AnchorPane();
-            Scene aboutScene = Constants.getScene(ap);
-            aboutScene.setOnContextMenuRequested(e -> getRightClickContextMenu().show(aboutScene.getWindow(), e.getScreenX(), e.getScreenY()));
-            aboutStage.setScene(aboutScene);
-            aboutStage.show();
-        });
-        menu.getItems().addAll(aboutDrifty);
         return menu;
     }
 
