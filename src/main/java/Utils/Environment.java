36960de8ecb73bddc2c0fdc16b95118ab6c1016d
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2023-10-23 09:20:06 +0530

    chore: Changed `.config` to `.drifty` directory for storing Drifty's data in macOS/Linux

--- src/main/java/Utils/Environment.java
+++ src/main/java/Utils/Environment.java
@@ -23,16 +23,16 @@ public class Environment {
     */
     public static void initializeEnvironment() {
         M.msgLogInfo("OS : " + OS.getOSName());
-        String ytDLP = OS.isWindows() ? "yt-dlp.exe" : OS.isMac() ? "yt-dlp_macos" : "yt-dlp";
-        String spotdl = OS.isWindows() ? "spotdl_win.exe" : OS.isMac() ? "spotdl_macos" : "spotdl_linux";
-        String appUseFolderPath = OS.isWindows() ? Paths.get(System.getenv("LOCALAPPDATA"), "Drifty").toAbsolutePath().toString() : Paths.get(System.getProperty("user.home"), ".config", "Drifty").toAbsolutePath().toString();
-        Program.setYt_DlpExecutableName(ytDLP);
-        Program.setSpotdlExecutableName(spotdl);
-        Program.setDriftyPath(appUseFolderPath);
+        String ytDlpExecName = OS.isWindows() ? "yt-dlp.exe" : OS.isMac() ? "yt-dlp_macos" : "yt-dlp";
+        String spotDLExecName = OS.isWindows() ? "spotdl_win.exe" : OS.isMac() ? "spotdl_macos" : "spotdl_linux";
+        String driftyFolderPath = OS.isWindows() ? Paths.get(System.getenv("LOCALAPPDATA"), "Drifty").toAbsolutePath().toString() : Paths.get(System.getProperty("user.home"), ".drifty").toAbsolutePath().toString();
+        Program.setYt_DlpExecutableName(ytDlpExecName);
+        Program.setSpotdlExecutableName(spotDLExecName);
+        Program.setDriftyPath(driftyFolderPath);
         CopyExecutables copyExecutables = new CopyExecutables();
         boolean ytDLPExists = false;
         try {
-            copyExecutables.copyExecutables(new String[]{ytDLP, spotdl});
+            copyExecutables.copyExecutables(new String[]{ytDlpExecName, spotDLExecName});
         } catch (IOException e) {
             M.msgInitError("Failed to copy yt-dlp! " + e.getMessage());
             M.msgInitError("Failed to copy spotDL! " + e.getMessage());
@@ -40,16 +40,16 @@ public class Environment {
         if (ytDLPExists && !isYtDLPUpdated()) {
             updateYt_dlp();
         }
-        File folder = new File(appUseFolderPath);
+        File folder = new File(driftyFolderPath);
         if (!folder.exists()) {
             try {
                 Files.createDirectory(folder.toPath());
-                M.msgInitInfo("Created Drifty folder : " + appUseFolderPath);
+                M.msgInitInfo("Created Drifty folder : " + driftyFolderPath);
             } catch (IOException e) {
-                M.msgInitError("Failed to create Drifty folder: " + appUseFolderPath + " - " + e.getMessage());
+                M.msgInitError("Failed to create Drifty folder: " + driftyFolderPath + " - " + e.getMessage());
             }
         } else {
-            M.msgInitInfo("Drifty folder already exists : " + appUseFolderPath);
+            M.msgInitInfo("Drifty folder already exists : " + driftyFolderPath);
         }
     }
 
@@ -77,6 +77,7 @@ public class Environment {
         long timeSinceLastUpdate = System.currentTimeMillis() - AppSettings.get.lastDLPUpdateTime();
         return timeSinceLastUpdate <= oneDay;
     }
+
     public static MessageBroker getMessageBroker() {
         return M;
     }
