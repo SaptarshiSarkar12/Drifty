244a4b2c0ff77e56fdac8da66e03041bf7922527
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2023-10-24 23:47:12 +0530

    fix: Applied fixes according to the linter

--- src/main/java/GUI/Forms/DownloadFile.java
+++ src/main/java/GUI/Forms/DownloadFile.java
@@ -421,9 +421,9 @@ public class DownloadFile extends Task<Integer> {
     public String getSpotifyDownloadLink(String link){
         sendInfoMessage("Trying to get download link for \"" + link + "\"");
         // Remove si parameter from the link
-        link = link.replaceAll("\\?si=.*", "");
+        this.link = link.replaceAll("\\?si=.*", "");
         String spotDLPath = Program.get(Program.SPOTDL);
-        ProcessBuilder processBuilder = new ProcessBuilder(spotDLPath, "url", link);
+        ProcessBuilder processBuilder = new ProcessBuilder(spotDLPath, "url", this.link);
         Process process;
         try {
             process = processBuilder.start();
