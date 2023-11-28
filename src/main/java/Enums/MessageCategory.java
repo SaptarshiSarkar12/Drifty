1f48faa45f177b32c1dca9b33bf88242e70eeb92
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2023-11-28 23:09:43 +0530

    feat: Added more ANSI colour coded printing in Drifty CLI

--- src/main/java/Enums/MessageCategory.java
+++ src/main/java/Enums/MessageCategory.java
@@ -4,5 +4,5 @@ package Enums;
  * This class is used to provide a category to the message that is sent to the Message Broker.
  */
 public enum MessageCategory {
-    LINK, DIRECTORY, DOWNLOAD, FILENAME, LOG, INITIALIZATION, STYLE, BATCH, SETTINGS
+    LINK, DIRECTORY, DOWNLOAD, FILENAME, LOG, INITIALIZATION, STYLE, BATCH, SETTINGS, INPUT, HISTORY
 }
