98f56e843eac23d33624a4526bfb5f6c13a5318a
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2023-12-22 18:13:57 +0530

    feat: Converted Drifty into a multi-module project (#398)
    * feat: Converted Drifty to a Modular application
    
    * feat: Added support for building native images for Drifty CLI and GUI in the new modular Drifty
    
    * fix: Removed open rewrite's CodeCleanup recipe as it is causing build issues
    
    * fix: Made the required changes in the Dockerfiles for the conversion of the whole project into a multi-modular application
    
    * fix(CI): Fixed all build and linter CI issues
    
    * fix(CI): Fixed linter and formatter issues
    
    * style: Formatted Java files
    
    * feat: Added icons for the Drifty GUI app made from modular Drifty
    
    * fix: Fixed build errors
    
    * fix: Added the fixes asked to be implemented, by Coderabbitai
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Core/src/main/java/properties/Mode.java
@@ -0,0 +1,19 @@
+package properties;
+
+public enum Mode {
+    CLI, GUI;
+
+    private static Mode mode = Mode.CLI;
+
+    public static void setGUIMode() {
+        Mode.mode = Mode.GUI;
+    }
+
+    public static boolean isGUI() {
+        return mode.equals(Mode.GUI);
+    }
+
+    public static boolean isCLI() {
+        return mode.equals(Mode.CLI);
+    }
+}
