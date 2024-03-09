2bfa02ca052803f29a53516f457f6be057d7836d
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2024-03-09 21:05:37 +0530

    feat: Added support for releasing beta releases

--- Website/app/Releases.js
+++ Website/app/Releases.js
@@ -42,7 +42,7 @@ function downloadLatestRelease(OSName, applicationType) {
 
 function downloadOlderReleases(OSName, applicationType, version) {
   if (applicationType === "CLI") {
-    if (OSName === "Windows") {
+    if (OSName === "Windows exe") {
       window.open(
         "https://github.com/SaptarshiSarkar12/Drifty/releases/download/" +
           version +
@@ -106,12 +106,13 @@ export default function Releases({ props }) {
     });
     return releases;
   }, [props.release]);
-  const filteredReleases = useMemo(() => {
+  const filteredReleases = useMemo(() => { // Starting from v2.0.0, separate executables for windows, linux and macOS are available. So, we need three buttons (in total) in that case.
     const releases = [];
     props.release.map((item, index) => {
       if (index !== 0) {
         if (
           item.tag_name.startsWith("v2") &&
+          item.prerelease === false &&
           releases.length <= maxReleasesToDisplay
         ) {
           releases.push(item);
@@ -129,7 +130,7 @@ export default function Releases({ props }) {
         noOfReleases < maxReleasesToDisplay &&
         index !== 0
       ) {
-        if (!item.tag_name.startsWith("v2")) {
+        if (!item.tag_name.startsWith("v2") && item.prerelease === false) {
           releases.push(item);
           noOfReleases++;
         }
@@ -243,23 +244,29 @@ export default function Releases({ props }) {
 
       <div>
         {filteredPreReleases.length !== 0 && (
-          <h1 className="select-none text-center font-bold text-2xl pt-10">
-            Beta Releases
-          </h1>
+          <>
+            <h1 className="select-none text-center font-bold text-2xl pt-10">
+              Beta Release
+            </h1>
+            <p className="text-center text-base pt-1 font-semibold text-gray-700">
+              Try out the latest features and improvements before the final release
+            </p>
+          </>
         )}
         {filteredPreReleases.map((item, index) => {
           index = index + filteredReleases.length;
           if (filteredPreReleases.length !== 0) {
             return (
-                <div
-                    key={index}
-                    className="text-center p-5 text-base font-normal"
-                >
+                <div key={index} className="text-center p-5 text-base font-normal">
                   <span className="font-bold">{item.tag_name} </span>
                   <p>
                     {new Date(item.published_at).toString()} with{" "}
                     {item.assets[0].download_count +
-                        item.assets[1].download_count}{" "}
+                        item.assets[1].download_count +
+                        item.assets[2].download_count +
+                        item.assets[3].download_count +
+                        item.assets[4].download_count +
+                        item.assets[5].download_count}{" "}
                     Downloads
                   </p>
                   <button
@@ -274,24 +281,62 @@ export default function Releases({ props }) {
                           dangerouslySetInnerHTML={{__html: content[index]}}
                       ></div>
                   )}
+
                   <div
-                      className="grid md:grid-flow-col md:gap-52 xs:gap-8 justify-center text-white mt-3 font-semibold">
+                      className="grid md:grid-flow-col  md:gap-16 xs:gap-3 justify-center text-white mt-3 font-semibold">
+                    <div className={"grid grid-cols-1 justify-items-center"}>
+                      <button
+                          className="select-none pl-3 pr-3 w-auto h-auto text-2xl bg-gradient-to-r from-blue-600 to-green-500 hover:from-pink-500 hover:to-yellow-500 rounded-full p-1 shadow-none hover:transition ease-in-out duration-300 delay-80 hover:-translate-y-1 hover:scale-110 hover:drop-shadow-2xl"
+                          onClick={() =>
+                              downloadOlderReleases(
+                                  "Windows exe",
+                                  applicationType,
+                                  item.tag_name
+                              )
+                          }
+                      >
+                        Download <i className="fab fa-brands fa-windows"></i>
+                      </button>
+                      {applicationType === "GUI" && (
+                          <button
+                              className={
+                                "text-sm text-violet-900 font-semibold hover:underline hover:transition ease-in-out duration-300 delay-80 hover:-translate-y-0.5 hover:scale-110"
+                              }
+                              onClick={() =>
+                                  downloadOlderReleases(
+                                      "Windows msi",
+                                      applicationType,
+                                      item.tag_name
+                                  )
+                              }
+                          >
+                            Prefer the msi?
+                          </button>
+                      )}
+                    </div>
                     <button
-                        className="select-none pl-3 pr-3 w-auto h-auto text-2xl bg-gradient-to-r from-blue-600 to-green-500 hover:from-pink-500 hover:to-yellow-500 rounded-full p-1 shadow-none hover:transition ease-in-out duration-300 delay-80 hover:-translate-y-1 hover:scale-110 hover:drop-shadow-2xl"
+                        className="select-none pl-3 pr-3 w-auto h-min text-2xl bg-gradient-to-r from-blue-600 to-green-500 hover:from-pink-500 hover:to-yellow-500 rounded-full p-1 shadow-none hover:transition ease-in-out duration-300 delay-80 hover:-translate-y-1 hover:scale-110 hover:drop-shadow-2xl"
                         onClick={() =>
-                            window.open(item.assets[1].browser_download_url)
+                            downloadOlderReleases(
+                                "Linux",
+                                applicationType,
+                                item.tag_name
+                            )
                         }
                     >
-                      Download <i className="fab fa-brands fa-windows"></i>
+                      Download <i className="fab fa-brands fa-linux"></i>
                     </button>
                     <button
-                        className="select-none pl-3 pr-3 w-auto h-auto text-2xl bg-gradient-to-r from-blue-600 to-green-500 hover:from-pink-500 hover:to-yellow-500 rounded-full p-1 shadow-none hover:transition ease-in-out duration-300 delay-80 hover:-translate-y-1 hover:scale-110 hover:drop-shadow-2xl"
+                        className="select-none pl-3 pr-3 w-auto h-min text-2xl bg-gradient-to-r from-blue-600 to-green-500 hover:from-pink-500 hover:to-yellow-500 rounded-full p-1 shadow-none hover:transition ease-in-out duration-300 delay-80 hover:-translate-y-1 hover:scale-110 hover:drop-shadow-2xl"
                         onClick={() =>
-                            window.open(item.assets[0].browser_download_url)
+                            downloadOlderReleases(
+                                "MacOS",
+                                applicationType,
+                                item.tag_name
+                            )
                         }
                     >
-                      Download <i className="fab fa-brands fa-apple"></i>{" "}
-                      <i className="fab fa-brands fa-linux"></i>
+                      Download <i className="fab fa-brands fa-apple"></i>
                     </button>
                   </div>
                 </div>
@@ -301,6 +346,9 @@ export default function Releases({ props }) {
         <h1 className="select-none text-center font-bold text-2xl pt-10">
           Past Releases
         </h1>
+        <p className="text-center text-base pt-1 font-semibold text-gray-700">
+          Download older releases of Drifty
+        </p>
         {filteredReleases.map((item, index) => {
           return (
               <div key={index} className="text-center p-5 text-base font-normal">
