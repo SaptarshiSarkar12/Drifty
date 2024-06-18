d14350af229f5ae0cc6c4c4985a7a082c94c5aef
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2024-06-18 20:51:36 +0530

    fix(Website): Fixed the poor contrast of the `Send Message` button in the Contact Us page, when hovered

--- Website/app/contact/page.js
+++ Website/app/contact/page.js
@@ -21,15 +21,16 @@ export default function contact() {
           action="https://formsubmit.co/e94b201f9a607081e9f9f8ee09ff5e25"
         >
           <input
-            className="bg-white focus:outline-blue-600 text-blue-600 outline:none caret-blue-600 md:w-6/12 sm:w-1/2 h-14 p-3 col-span-2"
+            className="bg-white rounded-md focus:outline-blue-600 text-blue-600 outline:none caret-blue-600 md:w-6/12 sm:w-1/2 h-14 p-3 col-span-2"
             type="text"
             name="name"
+            autoFocus={true}
             inputMode="text"
             required
             placeholder="Name"
           />
           <input
-            className="bg-white focus:outline-blue-600 outline:none caret-blue-600 mt-10 md:ml-auto md:w-1/2 justify-end h-14 md:col-span-1 col-span-2 p-2 text-blue-600"
+            className="bg-white rounded-md focus:outline-blue-600 outline:none caret-blue-600 mt-10 md:ml-auto md:w-1/2 justify-end h-14 md:col-span-1 col-span-2 p-2 text-blue-600"
             inputMode="email"
             pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/"
             type="email"
@@ -38,7 +39,7 @@ export default function contact() {
             placeholder="Email"
           />
           <input
-            className="bg-white focus:outline-blue-600 outline:none caret-blue-600 mt-10 md:mr-auto md:ml-1 md:w-1/2 justify-start  h-14 p-2  md:col-span-1 col-span-2 text-blue-600"
+            className="bg-white rounded-md focus:outline-blue-600 outline:none caret-blue-600 mt-10 md:mr-auto md:ml-1 md:w-1/2 justify-start  h-14 p-2  md:col-span-1 col-span-2 text-blue-600"
             minLength={6}
             pattern="/^([+]?\d{1,2}[-\s]?|)\d{3}[-\s]?\d{3}[-\s]?\d{4}$/"
             maxLength={26}
@@ -49,14 +50,14 @@ export default function contact() {
             placeholder="Phone Number"
           />
           <textarea
-            className="focus:outline-blue-600 outline:none caret-blue-600 md:w-6/12 sm:w-1/2 h-64 col-span-2 mt-10 p-6  text-blue-600 resize-none"
+            className="rounded-md focus:outline-blue-600 outline:none caret-blue-600 md:w-6/12 sm:w-1/2 h-64 col-span-2 mt-10 p-6  text-blue-600 resize-none"
             inputMode="text"
             pattern="text"
             name="message"
             placeholder="Message"
             required
           ></textarea>
-          <button className="mt-10 px-6 col-span-2 py-2 w-30 h-10 bg-blue-700 text-white font-medium text-lg leading-tight rounded-lg shadow-md hover:bg-yellow-400 hover:shadow-lg active:bg-blue-400 active:shadow-lg transition duration-100 ease-in-out">
+          <button className="mt-10 px-6 col-span-2 py-2 w-30 h-10 bg-blue-700 text-white font-medium text-lg leading-tight rounded-lg shadow-md hover:bg-yellow-400 hover:text-gray-800 hover:shadow-lg active:bg-blue-400 active:shadow-lg transition duration-100 ease-in-out">
             Send Message
           </button>
         </form>
