c16c88a016e81a6afcc954e2e61f8c2cde084ae6
Abhilash Reddy <abhilashreddysh.99@gmail.com>
2024-06-29 19:18:27 +0530

    Update Broken Links to Github
    Signed-off-by: Abhilash Reddy <abhilashreddysh.99@gmail.com>

--- Website/theme.config.jsx
+++ Website/theme.config.jsx
@@ -3,5 +3,31 @@ export default {
   project: {
     link: "https://github.com/SaptarshiSarkar12/Drifty",
   },
-  // ... other theme options
+  docsRepositoryBase:
+    "https://github.com/SaptarshiSarkar12/Drifty/tree/master/docs",
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s | Drifty",
+    };
+  },
+  toc: {
+    backToTop: true,
+    float: true,
+  },
+  sidebar: { toggleButton: true, autoCollapse: true },
+  navigation: true,
+  search: {
+    placeholder: "Search ",
+  },
+  footer: {
+    text: (
+      <span>
+        © {new Date().getFullYear()}{" "}
+        <a href="/" target="_blank">
+          Drifty
+        </a>
+        . All Rights Reserved.
+      </span>
+    ),
+  },
 };
