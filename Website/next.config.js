fecaa0909094fb350eb9fa11921b79f58f612e83
Abhilash Reddy <abhilashreddysh.99@gmail.com>
2024-08-20 07:43:07 +0530

    merge conflicts 2
    Signed-off-by: Abhilash Reddy <abhilashreddysh.99@gmail.com>

--- Website/next.config.js
+++ Website/next.config.js
@@ -1,30 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.BASE_PATH,
-  output: "export",
-  images: {
-    minimumCacheTTL: 60 * 60 * 24,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "camo.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.jsdelivr.net",
-      },
-    ],
-    dangerouslyAllowSVG: true,
-    unoptimized: true,
-  },
+	basePath: process.env.BASE_PATH,
+	output: "export",
+	images: {
+		minimumCacheTTL: 60 * 60 * 24,
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+			},
+			{
+				protocol: "https",
+				hostname: "camo.githubusercontent.com",
+			},
+			{
+				protocol: "https",
+				hostname: "cdn.jsdelivr.net",
+			},
+		],
+		dangerouslyAllowSVG: true,
+		unoptimized: true,
+	},
 };
 const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.jsx",
+	theme: "nextra-theme-docs",
+	themeConfig: "./theme.config.jsx",
 });
 module.exports = withNextra(nextConfig);
 // module.exports = nextConfig
