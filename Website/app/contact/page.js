8a89a347a97c1e6fe5ccf052e14ac97329e0a9b7
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-10-01 11:53:56 +0530

    Added contact us page

--- Website/app/contact/page.js
+++ Website/app/contact/page.js
@@ -10,18 +10,24 @@ export default function contact() {
   return (
     <div>
       <Header props={"bg-top"} />
-      <div className="select-none bg-gradient-to-b from-top via-about to-bottom py-5">
-        <h1 className="text-center font-semibold text-7xl">Contact Us</h1>
-        <p className="text-center font-normal pt-5 pb-8 text-lg">
-          We&apos;d love to hear from you!
-        </p>
+     
+      
+
+      <div className="select-none bg-gradient-to-b from-top via-about to-bottom ">
+      
+        <div className="flex justify-center items-center  w-full max-md:p-2   p-12">
+
         <form
-          className="grid grid-cols-2 justify-items-center"
+          className="md:w-3/5   bg-white rounded-2xl shadow-lg shadow-gray-500 p-14" 
           method="POST"
           action="https://formsubmit.co/e94b201f9a607081e9f9f8ee09ff5e25"
         >
+                      <h1 className="  mb-4 text-5xl font-bold">Contact Us</h1>
+                      <p className="">          We&apos;d love to hear from you!
+                      </p>
+                      <div className="flex justify-around gap-2 mt-6 max-md:flex-col">
           <input
-            className="bg-white rounded-md focus:outline-blue-600 text-blue-600 outline:none caret-blue-600 md:w-6/12 sm:w-1/2 h-14 p-3 col-span-2"
+            className= "border max-md:w-full rounded-full max-w-[400px] p-2 w-1/2 bg-white  focus:outline-blue-600 text-blue-600 outline:none caret-blue-600 md:w-6/12 sm:w-1/2  h-12  col-span-2"
             type="text"
             name="name"
             autoFocus={true}
@@ -29,8 +35,8 @@ export default function contact() {
             required
             placeholder="Name"
           />
-          <input
-            className="bg-white rounded-md focus:outline-blue-600 outline:none caret-blue-600 mt-10 md:ml-auto md:w-1/2 justify-end h-14 md:col-span-1 col-span-2 p-2 text-blue-600"
+          <input 
+            className="border max-md:w-full max-md:mt-6  max-w-[400px] rounded-full p-2 w-1/2 bg-white  focus:outline-blue-600 outline:none caret-blue-600  h-12  md:ml-auto md:w-1/2 justify-end  md:col-span-1 col-span-2  text-blue-600"
             inputMode="email"
             pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/"
             type="email"
@@ -38,8 +44,9 @@ export default function contact() {
             required
             placeholder="Email"
           />
+          </div>
           <input
-            className="bg-white rounded-md focus:outline-blue-600 outline:none caret-blue-600 mt-10 md:mr-auto md:ml-1 md:w-1/2 justify-start  h-14 p-2  md:col-span-1 col-span-2 text-blue-600"
+            className="border rounded-full p-2 w-full my-2 bg-white  focus:outline-blue-600 outline:none caret-blue-600 mt-6 md:mr-auto md:ml-1 justify-start  h-12   md:col-span-1 col-span-2 text-blue-600"
             minLength={6}
             pattern="/^([+]?\d{1,2}[-\s]?|)\d{3}[-\s]?\d{3}[-\s]?\d{4}$/"
             maxLength={26}
@@ -50,17 +57,20 @@ export default function contact() {
             placeholder="Phone Number"
           />
           <textarea
-            className="rounded-md focus:outline-blue-600 outline:none caret-blue-600 md:w-6/12 sm:w-1/2 h-64 col-span-2 mt-10 p-6  text-blue-600 resize-none"
+            className="border rounded-lg w-full my-2  focus:outline-blue-600 outline:none caret-blue-600  min-h-[150px] col-span-2 mt-6 p-4  text-blue-600 "
             inputMode="text"
             pattern="text"
             name="message"
-            placeholder="Message"
+            placeholder="How Can I Help You ?"
             required
           ></textarea>
-          <button className="mt-10 px-6 col-span-2 py-2 w-30 h-10 bg-blue-700 text-white font-medium text-lg leading-tight rounded-lg shadow-md hover:bg-yellow-400 hover:text-gray-800 hover:shadow-lg active:bg-blue-400 active:shadow-lg transition duration-100 ease-in-out">
+          <button className=" rounded-3xl mt-10 hover:bg-yellow-400 hover:text-gray-800 hover:shadow-lg active:shadow-lg w-full bg-blue-600 text-white p-3   ease-in-out">
             Send Message
           </button>
+          <p className="text-lg mt-10 text-center ">By contacting us you agree to our <b>Terms of Service</b> and <b>Privacy Policy</b>.</p>
+
         </form>
+        </div>
       </div>
       <Footer />
     </div>
