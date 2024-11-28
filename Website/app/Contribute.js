5d1035f271f93edcabd52a359b78abe69d5df64e
Fabricio Jordan <39861923+FabrizioJordan@users.noreply.github.com>
2024-11-28 04:25:01 -0300

    feat: Revamp the website with modern design (#721)
    * "Redesign of buttons, correction of bad practices. A more modern view of the website."
    
    Signed-off-by: FabrizioJordan <dev.fjordan@gmail.com>
    
    * "Responsive size in mobile header buttons and minor changes"
    
    Signed-off-by: FabrizioJordan <dev.fjordan@gmail.com>
    
    * "Completely removed scaling on hover"
    
    Signed-off-by: FabrizioJordan <dev.fjordan@gmail.com>
    
    * chore(maven): bump commons-io:commons-io from 2.17.0 to 2.18.0 (#722)
    
    Bumps commons-io:commons-io from 2.17.0 to 2.18.0.
    
    ---
    updated-dependencies:
    - dependency-name: commons-io:commons-io
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(maven): bump org.openrewrite.recipe:rewrite-recommendations from 1.12.0 to 1.13.0 (#723)
    
    * Overflow-hidden corrected
    
    Signed-off-by: FabrizioJordan <dev.fjordan@gmail.com>
    
    * Lint error corrected
    
    Signed-off-by: FabrizioJordan <dev.fjordan@gmail.com>
    
    * "Updated styles and layout for Header and MainSection components"
    
    Signed-off-by: FabrizioJordan <dev.fjordan@gmail.com>
    
    * chore: Formatted the files using prettier
    
    Signed-off-by: Saptarshi Sarkar <saptarshi.programmer@gmail.com>
    
    ---------
    
    Signed-off-by: FabrizioJordan <dev.fjordan@gmail.com>
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Saptarshi Sarkar <saptarshi.programmer@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Saptarshi Sarkar <saptarshi.programmer@gmail.com>
    Co-authored-by: FabrizioJordan <dev.fjordan@gmail.com>

--- Website/app/Contribute.js
+++ Website/app/Contribute.js
@@ -19,13 +19,13 @@ export default function Contribute({ props }) {
   return (
     <div
       id="contribute"
-      className="md:grid md:grid-cols-2 justify-center bg-var select-none text-white pb-5"
+      className="flex flex-col justify-center items-center gap-12 py-12 px-4  bg-var select-none text-white"
     >
-      <div className="md:pr-8 mb-2">
-        <h2 className="text-3xl mb-3 sm:text-4xl font-bold lg:mt-16 md:mt-10 sm:pt-10 sm:mb-10 xs:p-5 text-center">
+      <div className="flex flex-col gap-4">
+        <h2 className="text-3xl sm:text-4xl font-bold text-center">
           More Information About Drifty
         </h2>
-        <p className="text-2xl text-center ml-5 mr-5 font-sans mb-8 leading-normal xs:px-4">
+        <p className="max-w-2xl text-xl lg:text-2xl text-center font-sans leading-normal">
           It is available both in CLI (Command Line Interface) and GUI
           (Graphical User Interface) mode for all the major platforms like
           Windows, Linux and MacOS. We believe in team work. Any contribution
@@ -52,18 +52,18 @@ export default function Contribute({ props }) {
           and completed.
         </p>
       </div>
-      <div className="ml-2">
-        <h2 className="text-3xl mb-3 sm:text-4xl font-bold lg:mt-16 md:mt-10 sm:pt-10 sm:mb-10 xs:p-5 text-center">
+      <div className="flex flex-col gap-8">
+        <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-center">
           Be a part of Drifty Family!
         </h2>
-        <div className="flex items-center justify-center gap-[2em] flex-wrap rounded-full space-x-0 gap-y-4 justify-items-center xs:pb-5 pr-1">
+        <div className="flex items-center justify-center gap-[2em] flex-wrap rounded-full gap-y-4 justify-items-center xs:pb-5 pr-1">
           {props.contrib.map((item, index) => {
             if (index < 7 && item.type === "User") {
               // We are using 7 instead of 6 because we are not counting the dependabot
               return (
                 <a href={item.html_url} key={index}>
                   <Image
-                    className="rounded-full hover:transition ease-in-out hover:-translate-y-1 hover:scale-110 delay-150 duration-300"
+                    className="rounded-full border-2 border-transparent hover:transition ease-in-out hover:border-[#ffffff70] delay-150 duration-300"
                     width={64}
                     height={64}
                     src={item.avatar_url + ".webp&s=77"}
@@ -77,27 +77,27 @@ export default function Contribute({ props }) {
             href="https://github.com/SaptarshiSarkar12/Drifty/graphs/contributors"
             target="_blank"
           >
-            <div className="gap-1 rounded-full h-16 w-16 text-xl font-semibold flex items-center justify-center border-2 md:border-white text-center   hover:text-black xs:transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
+            <div className="py-2 px-3.5 gap-1 rounded-full h-16 w-16 text-xl font-semibold flex items-center justify-center text-center xs:transition ease-in-out delay-150 bg-blue-500 hover:bg-[#2e51ab] duration-300">
               {totalNoOfContributors - 6}+
             </div>
           </a>
         </div>
-        <div className="flex items-center justify-center flex-wrap text-lg font-semibold pr-3 gap-6 mt-4">
+        <div className="flex items-center justify-center flex-wrap text-lg font-semibold gap-6">
           <a
-            className="rounded-lg xs:p-[3px] sm:p-1 w-auto lg:w-48 border-2 md:border-white text-center hover:text-black xs:transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+            className="py-2 px-3.5 rounded-lg w-auto text-center xs:transition ease-in-out delay-100 bg-blue-500 hover:bg-[#2e51ab] duration-300"
             href="https://github.com/SaptarshiSarkar12/Drifty"
           >
             <i aria-hidden="true" className="fab fa-github p-2"></i>Contribute
           </a>
           <a
-            className="rounded-lg xs:p-[3px] sm:p-1 w-auto lg:w-48 border-2 md:border-white text-center hover:text-black xs:transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+            className="py-2 px-3.5 rounded-lg w-auto text-center xs:transition ease-in-out delay-100 bg-blue-500 hover:bg-[#2e51ab] duration-300"
             href="https://github.com/SaptarshiSarkar12/Drifty/stargazers"
           >
             <i aria-hidden="true" className="fa fa-regular fa-star p-2"></i>
             Stars : {values[0]}
           </a>
           <a
-            className="rounded-lg xs:p-[3px] sm:p-1 w-auto lg:w-48 border-2 md:border-white text-center hover:text-black xs:transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+            className="py-2 px-3.5 rounded-lg w-auto text-center xs:transition ease-in-out delay-100 bg-blue-500 hover:bg-[#2e51ab] duration-300"
             href="https://github.com/SaptarshiSarkar12/Drifty/network/members"
           >
             <i
