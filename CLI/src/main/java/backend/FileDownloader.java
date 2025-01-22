58ed107ae702032e86c30ca4c8936945e819ad87
Michał Ryniewicz <misiekxyz18@gmail.com>
2025-01-22 22:14:23 +0100

    Code fixes based on CodeQL feedback
    Signed-off-by: Michał Ryniewicz <misiekxyz18@gmail.com>

--- CLI/src/main/java/backend/FileDownloader.java
+++ CLI/src/main/java/backend/FileDownloader.java
@@ -95,9 +95,7 @@ public class FileDownloader implements Runnable {
                         for (File tempFile : tempFiles) {
                             Files.deleteIfExists(tempFile.toPath());
                         }
-                        for (FileOutputStream fileOutputStream : fileOutputStreams) {
-                            fileOutputStream.close();
-                        }
+                        fileOutputStreams.stream().close();
                     } else {
                         InputStream urlStream = url.openStream();
                         readableByteChannel = Channels.newChannel(urlStream);
