d4c9f6338db0cd34b77296260081e9e14e85582f
Abhilash Reddy <abhilashreddysh.99@gmail.com>
2025-03-08 19:34:25 +0530

    chore: migrate images.domains to images.remotePatterns in next.config.js
    Signed-off-by: Abhilash Reddy <abhilashreddysh.99@gmail.com>

--- Website/next.config.ts
+++ Website/next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+    ],
   },
 };
 
