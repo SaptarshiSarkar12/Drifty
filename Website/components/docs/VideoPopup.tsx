211222ceb689cc52a34e646669a2ae9ed96dd2f9
Abhilash Reddy S H <116741403+abhilashreddysh@users.noreply.github.com>
2025-03-25 10:20:46 +0530

    fix: Add dark mode support for demo popup window
    Signed-off-by: Abhilash Reddy S H <116741403+abhilashreddysh@users.noreply.github.com>

--- Website/components/docs/VideoPopup.tsx
+++ Website/components/docs/VideoPopup.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { Cards } from "nextra/components";
 
 type VideoPopupProps = {
   videoUrl: string;
@@ -15,31 +14,29 @@ export default function VideoPopup({
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="flex justify-center">
-      <Cards>
+    <div className="flex justify-center w-full">
+      <div className="w-full max-w-xs sm:max-w-sm md:max-w-md">
         <button
-          className="px-4 py-2 bg-(--button-bg) text-(--button-text) rounded-md shadow-md hover:bg-(--button-text) hover:text-(--button-bg) transition"
+          className="w-full px-4 py-2 bg-(--button-bg) text-(--button-text) rounded-md shadow-md hover:bg-(--button-text) hover:text-(--button-bg) transition text-sm sm:text-base"
           onClick={() => setOpen(true)}
         >
           {buttonText}
         </button>
-      </Cards>
+      </div>
 
       <div
-        className={`fixed inset-0 flex items-center justify-center bg-black/80 p-4 transition-opacity duration-100 ${
+        className={`fixed inset-0 flex items-center justify-center dark:bg-(--light-accent)/20 bg-(--dark-accent)/20 p-4 transition-opacity duration-100 ${
           open
             ? "opacity-100 scale-100"
             : "opacity-0 scale-90 pointer-events-none"
         }`}
         aria-hidden={!open}
       >
-        <div className="relative w-full sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-3xl bg-white rounded-xl shadow-lg transform transition-all">
-          <div className="flex items-center justify-between p-4 border-b border-gray-200">
-            <h3 className="text-lg font-semibold text-gray-900">
-              {buttonText}
-            </h3>
+        <div className="relative w-full sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-3xl dark:bg-(--dark-bg) bg-(--light-bg) rounded-xl shadow-lg transform transition-all">
+          <div className="flex items-center justify-between p-4 border-b border-(--button-bg)">
+            <h3 className="text-lg font-semibold">{buttonText}</h3>
             <button
-              className="text-gray-500 hover:bg-gray-200 rounded-full p-3 w-12 h-12 focus:outline-none focus:ring-2 focus:ring-gray-400"
+              className="text-(--button-bg) hover:bg-(--button-bg)/20 rounded-full p-3 w-12 h-12 focus:outline-none focus:ring-2 focus:ring-(--button-bg)"
               onClick={() => setOpen(false)}
             >
               ✕
