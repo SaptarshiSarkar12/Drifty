794ed601da02a9421f19a7117c029d98a6276252
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2025-06-17 13:42:38 +0530

    fix: Fixed disappearance of mermaid diagrams by using svg alternatives
    Signed-off-by: Saptarshi Sarkar <saptarshi.programmer@gmail.com>

---
+++ Website/components/docs/ThemeImage.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useTheme } from 'next-themes'
+import Image, { ImageProps } from 'next/image'
+import { useEffect, useState } from 'react'
+
+interface ThemeImageProps extends Omit<ImageProps, 'src'> {
+    lightSrc: string
+    darkSrc: string
+}
+
+export default function ThemeImage({ lightSrc, darkSrc, alt, ...props }: ThemeImageProps) {
+    const { theme, systemTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) return null
+
+    const resolvedTheme = theme === 'system' ? systemTheme : theme
+    const src = resolvedTheme === 'dark' ? darkSrc : lightSrc
+
+    return <Image src={src} alt={alt} {...props} />
+}
