8a2767369f8afc0b40f11585feb1101ecbc4fbc3
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2025-07-13 21:14:29 +0530

    test: Added tests for checking functionality of link type detection and link sanitization
    Signed-off-by: Saptarshi Sarkar <saptarshi.programmer@gmail.com>

--- CLI/src/main/java/module-info.java
+++ CLI/src/main/java/module-info.java
@@ -4,4 +4,6 @@ module CLI {
     requires org.apache.commons.io;
     requires com.google.gson;
     requires java.sql;
+
+    exports cli.init; // Exporting the "cli.init" package (in the "test" directory) for CLI Tests
 }
